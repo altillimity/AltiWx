14819ccce531a9f61f339d729c8bca5c3f195e68
Aang23 <qwerty15@gmx.fr>
2020-06-23 14:20:46 +0200

    Add Gitlab CI

--- scripts/dsb-noaa.lua
+++ scripts/dsb-noaa.lua
@@ -1,6 +1,6 @@
 logger:info("Processing NOAA DSB data...")
 
-output_file = filename .. ".txt"
+frame_file = filename .. ".txt"
 
 outflag = ""
 if (northbound) then
@@ -10,10 +10,18 @@ if (southbound) then
     outflag = "-S"
 end
 
-command = "demodPOES -s 48 '" .. input_file .. "' -o '" .. output_file .. "'"
+command = "demodPOES -s 48 '" .. input_file .. "' -o '" .. frame_file .. "'"
 
 logger:debug(command)
 local cmd_output = os.execute(command)
 if (not cmd_output == 0) then logger:error("demodPOES command failed!") end
+
+command = "java -jar NOAA_HIRS_Decoder.jar config.ini '" .. frame_file .. "'"
+
+logger:debug(command)
+local cmd_output = os.execute(command)
+if (not cmd_output == 0) then logger:error("NOAA_HIRS_Decoder command failed!") end
+
+output_file = filename .. "/msa.png"
 
 logger:info("Done processing NOAA DSB data!");
