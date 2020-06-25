2be5ee084b15594c452ce6fbf8c9316b1814ba81
Aang23 <qwerty15@gmx.fr>
2020-06-25 19:22:36 +0200

    lrpt fix

--- scripts/lrpt-meteorm2.py
+++ scripts/lrpt-meteorm2.py
@@ -1,5 +1,6 @@
 import altiwx
 import subprocess
+import os
 
 temp_file = altiwx.filename + ".wav"
 command = "sox -t raw -e floating-point -b 32 -c 2 -r " + altiwx.samplerate + " '" + altiwx.input_file + "' -t wav -e signed-integer -b 16 -c 2 -r " + altiwx.samplerate + " '" + temp_file + "'"
@@ -10,6 +11,8 @@ lrpt_file = altiwx.filename + ".lrpt"
 command = "meteor_demod -q -B -s " + altiwx.samplerate + " '" + temp_file + "' -o '" + lrpt_file + "'"
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
+
+os.remove(lrpt_file)
 
 output_file = altiwx.filename + ".png"
 command = "medet '" + lrpt_file + "' '" + altiwx.filename + "' -r 65 -g 65 -b 64 > /dev/null"
