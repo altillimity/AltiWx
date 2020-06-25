92ac9d404cd7c9993ffe9d2c85c9b94e26fb2e8c
Aang23 <qwerty15@gmx.fr>
2020-06-25 21:28:46 +0200

    Fix dsb-noaa.py

--- scripts/dsb-noaa.py
+++ scripts/dsb-noaa.py
@@ -6,7 +6,7 @@ command = "demodPOES -s 48 '" + altiwx.input_file + "' -o '" + frame_file + "'"
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
 
-output_file = filename + "/msa.png"
+output_file = altiwx.filename + "/msa.png"
 command = "java -jar NOAA_HIRS_Decoder.jar config.ini '" + frame_file + "'"
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
