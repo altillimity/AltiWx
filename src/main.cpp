3f7302d342add9a9cf3ee55fa30a88a051d185ca
Aang23 <qwerty15@gmx.fr>
2020-08-23 18:34:18 +0200

    Basic versioning

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     logger->info("/_/ |_/_/\\__/_/ |__/|__//_\\_\\ ");
     logger->info("                              ");
 
-    logger->info("Starting AltiWx...");
+    logger->info("Starting AltiWx v" + (std::string)ALTIWX_VERSION + "...");
     // Init config
     initConfig();
     // Set custom log level
