3cee490e974f493ec2681133006308375c06efd9
Aang23 <qwerty15@gmx.fr>
2021-02-03 20:59:45 +0100

    Fixup DSB script #2

--- scripts/dsb-noaa.py
+++ scripts/dsb-noaa.py
@@ -4,7 +4,7 @@ import os
 
 altiwx.info("Processing NOAA DSB data...")
 
-command = "(cd " + os.path.split(os.path.abspath(altiwx.input_file))[0] + " NOAA-HIRS-Decoder '" + altiwx.input_file + "' dsb)"
+command = "(cd '" + os.path.split(os.path.abspath(altiwx.input_file))[0] + "' && NOAA-HIRS-Decoder '" + altiwx.input_file + "' dsb)"
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
 
