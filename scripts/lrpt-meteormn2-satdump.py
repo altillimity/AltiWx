2254ea65fc51d53c86c8d863527c485c4eead1be
FelixTRG <61870387+OK9UWU@users.noreply.github.com>
2022-11-22 18:38:47 +0100

    Update lrpt-meteormn2-satdump.py

--- scripts/lrpt-meteormn2-satdump.py
+++ scripts/lrpt-meteormn2-satdump.py
@@ -1,10 +1,12 @@
 import altiwx
 import subprocess
+import os
+
 
 altiwx.info("Processing METEOR-M 2 LRPT data...")
 
 output_folder = altiwx.filename
-System.IO.Directory.CreateDirectory(output_folder)
+os.mkdir(output_folder)
 command = "satdump meteor_m2_lrpt soft '" + altiwx.input_file + "' '" + output_folder + "' > /dev/null"
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
