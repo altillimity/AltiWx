0ab6d858301723ca02794c3b4a16739f5b20c437
OK9UWU <roobiso414@gmail.com>
2023-01-26 21:54:52 +0100

    Solve filename collisions

--- src/plugin/plugins.h
+++ src/plugin/plugins.h
@@ -2,7 +2,7 @@
 #include <memory>
 #include "api/altiwx/plugin.h"
 
-#define PLUGIN_FOLDER_PATH "plugins"
+#define PLUGIN_FOLDER_PATH "altiwx_plugins"
 
 std::shared_ptr<altiwx::Plugin> loadPlugin(std::string plugin);
 void initPlugins();
