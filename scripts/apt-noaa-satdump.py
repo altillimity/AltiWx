f38d4d1a0363ce726dee791a0a89c79b063eeb52
sdrgeek <sdrgeek@protonmail.com>
2023-08-17 17:47:00 +0200

    add SatDump APT processing script

---
+++ scripts/apt-noaa-satdump.py
@@ -0,0 +1,11 @@
+import altiwx
+import subprocess
+
+
+altiwx.info("Processing NOAA APT data...")
+
+command = "satdump noaa_apt wav '" + altiwx.input_file + "' '" + altiwx.filename + "' --satellite_number " + altiwx.satellite_name.replace('NOAA ', '')
+altiwx.debug(command)
+subprocess.Popen([command], shell=1).wait()
+
+altiwx.info("Done processing NOAA APT data!")
