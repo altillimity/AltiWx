bee8b135d31131338bc7d4357b4c077cbd840ac5
sdrgeek <81026491+sdrgeek@users.noreply.github.com>
2023-11-15 14:28:37 +0100

    Update apt-noaa-satdump.py
    Input level name changed from wav -> audio_wav

--- scripts/apt-noaa-satdump.py
+++ scripts/apt-noaa-satdump.py
@@ -4,7 +4,7 @@ import subprocess
 
 altiwx.info("Processing NOAA APT data...")
 
-command = "satdump noaa_apt wav '" + altiwx.input_file + "' '" + altiwx.filename + "' --satellite_number " + altiwx.satellite_name.replace('NOAA ', '')
+command = "satdump noaa_apt audio_wav '" + altiwx.input_file + "' '" + altiwx.filename + "' --satellite_number " + altiwx.satellite_name.replace('NOAA ', '')
 altiwx.debug(command)
 subprocess.Popen([command], shell=1).wait()
 
